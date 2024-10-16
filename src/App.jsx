efc2cc6d06d9f5e14963672ea2b46821394e4492
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2024-10-16 00:28:20 -0700

    Refactor App component to add CardButton and render multiple JournalItems

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import CardButton from "./components/CardButton/CardButton";
 import JournalItem from "./components/JournalItem/JournalItem";
 
 function App() {
@@ -21,13 +22,16 @@ function App() {
       <h1>Header</h1>
       <p>Some text</p>
       <Button />
+      <CardButton>New memo</CardButton>
       {data.map((entry, index) => (
-        <JournalItem
-          key={index}
-          title={entry.title}
-          date={entry.date}
-          text={entry.text}
-        />
+        <CardButton>
+          <JournalItem
+            key={index}
+            title={entry.title}
+            date={entry.date}
+            text={entry.text}
+          />
+        </CardButton>
       ))}
     </>
   );
