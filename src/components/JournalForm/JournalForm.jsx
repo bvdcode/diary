b3e7246f8f9332ec99fc1f19085912aa5a57d1e6
Vadim Belov <github@belov.us>
2024-12-08 01:58:09 -0800

    Enhance JournalForm to improve focus handling on validation errors using refs

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,16 +1,30 @@
-import { useEffect, useReducer } from "react";
 import classnames from "classnames";
 import Button from "../Button/Button";
 import styles from "./JournalForm.module.css";
+import { useEffect, useReducer, useRef } from "react";
 import { INITIAL_STATE, formReducer } from "./JournalForm.state";
 
 function JournalForm({ onSubmit }) {
   const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
   const { isFormValid, isValid, values } = formState;
+  const titleRef = useRef();
+  const dateRef = useRef();
+  const textRef = useRef();
+
+  const focusError = (isValid) => {
+    if (!isValid.title) {
+      titleRef.current.focus();
+    } else if (!isValid.date) {
+      dateRef.current.focus();
+    } else if (!isValid.text) {
+      textRef.current.focus();
+    }
+  };
 
   useEffect(() => {
     let timerId;
     if (!isValid.date || !isValid.post || !isValid.title) {
+      focusError(isValid);
       timerId = setTimeout(() => {
         dispatchForm({ type: "RESET_VALIDITY" });
       }, 700);
@@ -45,6 +59,7 @@ function JournalForm({ onSubmit }) {
           type="text"
           value={values.title}
           onChange={onChange}
+          ref={titleRef}
           name="title"
           placeholder="Title"
           className={classnames(styles.input, {
@@ -61,6 +76,7 @@ function JournalForm({ onSubmit }) {
           type="date"
           value={values.date}
           onChange={onChange}
+          ref={dateRef}
           id="date"
           name="date"
           placeholder="Date"
@@ -88,6 +104,7 @@ function JournalForm({ onSubmit }) {
         name="text"
         value={values.text}
         onChange={onChange}
+        ref={textRef}
         placeholder="What's on your mind?"
         cols="30"
         rows="10"
